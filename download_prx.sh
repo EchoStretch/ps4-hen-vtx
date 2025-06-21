c6b6835a6f3bf7c695f44b4a57a0a138aa4e22f4
illusiony <37698908+illusion0001@users.noreply.github.com>
2025-06-21 22:50:53 +1200

    Upload prx to disk

---
+++ download_prx.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+c=$PWD
+cd installer/build
+f=plugins.zip
+curl -fLJO https://github.com/illusion0001/ps4-hen-plugins/releases/latest/download/$f
+unzip $f
+cd bin/plugins/prx_final
+for file in *.prx; do xxd -i "$file" > "$c/installer/include/${file}.inc"; done
+cd $c
