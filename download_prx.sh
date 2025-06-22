c8b9a021081afe68a1844954867d3adf8f7a4da3
illusiony <37698908+illusion0001@users.noreply.github.com>
2025-06-22 20:12:41 +1200

    download_prx: fix path

--- download_prx.sh
+++ download_prx.sh
@@ -7,6 +7,5 @@ cd $b
 f=plugins.zip
 curl -fLJO https://github.com/illusion0001/ps4-hen-plugins/releases/latest/download/$f
 unzip $f
-cd bin/plugins/prx_final
 for file in *.prx; do xxd -i "$file" > "$c/installer/include/${file}.inc"; done
 cd $c
